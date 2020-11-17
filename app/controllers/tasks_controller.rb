bc3683bc02ce4c1036a9f2722c86517af356a302
haruka uede <happ_yjp@yahoo.co.jp>
2020-11-17 20:34:08 +0000

    after review2 login, has_many

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
-    before_action :set_task, only: [:show, :edit, :update, :destroy]
+    # before_action :set_task, only: [:show, :edit, :update, :destroy]
     before_action :require_user_logged_in
-    # before_action :correct_user, only: [:show, :edit, :update, :destroy]
+    before_action :correct_user, only: [:show, :edit, :update, :destroy]
     
     def index
         # @tasks = Task.all
@@ -53,18 +53,18 @@ class TasksController < ApplicationController
     
     private
     
-    def set_task
-        @task = Task.find(params[:id])
-    end
+    # def set_task
+    #     @task = Task.find(params[:id])
+    # end
     
     def task_params
         params.require(:task).permit(:content, :status)
     end
     
-    # def correct_user
-    #     @task = current_user.tasks.find_by(id: params[:id])
-    #     unless @task
-    #         redirect_to root_url
-    #     end
-    # end
+    def correct_user
+        @task = current_user.tasks.find_by(id: params[:id])
+        unless @task
+            redirect_to root_url
+        end
+    end
 end
